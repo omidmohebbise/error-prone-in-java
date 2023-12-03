2eae7a63a0ad380bd1a735dd6d29a12fcce6253d
omid mohebbi <omidmohebbise@gmail.com>
2023-12-03 13:04:22 +0100

    add examples and README.md for first usage in java

---
+++ README.md
@@ -0,0 +1,5 @@
+# ErrorProne Java Project
+This project demonstrates the use of the ErrorProne plugin in a Java project to catch common programming errors at compile time. ErrorProne is a static analysis tool that helps identify and prevent potential issues in your code.
+
+
+ErrorProne is a powerful tool for improving code quality by detecting and preventing common programming mistakes. This project includes examples of code snippets that might result in runtime errors or unintended behavior. By using ErrorProne, these issues can be identified during the compilation process, allowing developers to address them early in the development lifecycle.
--- src/main/java/com/omidmohebbise/Main.java
+++ src/main/java/com/omidmohebbise/Main.java
@@ -1,7 +1,28 @@
 package com.omidmohebbise;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String[] args) {
+        //fromArgumentError();
+        formatDate();
+    }
+
+    private static void fromArgumentError() {
+        Set<Short> s = new HashSet<>();
+        for (short i = 0; i < 100; i++) {
+            s.add(i);
+            s.remove(i - 1);
+        }
+        System.out.println(s.size());
+    }
+
+    public static void formatDate() {
+        Date date = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("MM-dd-yyyy");
+        System.out.println("Format Date: " + ft.format(date));
     }
 }
